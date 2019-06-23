560e84a83ed3fe2e29fc543d2babde1c2e33ced1
Axel Marko <axkmar@gmail.com>
2019-06-23 18:33:39 +0300

    Add first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_135119) do
+ActiveRecord::Schema.define(version: 2019_06_23_152939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_06_23_135119) do
     t.string "name"
     t.text "description"
     t.integer "price"
-    t.string "type"
+    t.string "category"
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_06_23_135119) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
