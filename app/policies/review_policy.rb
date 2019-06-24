f032b064bd3fad9ff2b062b5ae3b7abbd15582ae
Axel Marko <axkmar@gmail.com>
2019-06-24 13:27:51 +0300

    Implement Pundit for Reviews

---
+++ app/policies/review_policy.rb
@@ -0,0 +1,19 @@
+class ReviewPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user || user.admin
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_164259) do
+ActiveRecord::Schema.define(version: 2019_06_24_093116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_06_23_164259) do
     t.bigint "performance_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["performance_id"], name: "index_reviews_on_performance_id"
   end
 
