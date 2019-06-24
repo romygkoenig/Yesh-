3410fb94ce4593c9baca4269dc66b68b3ebd72c8
Valentina Heimann <vheimann2@googlemail.com>
2019-06-24 12:38:00 +0300

    Added methods to BookingsController as well as Pundit authorization

--- app/controllers/account/bookings_controller.rb
+++ app/controllers/account/bookings_controller.rb
@@ -1,7 +1,7 @@
 class Account::BookingsController < ApplicationController
 
   def index
-    @bookings = current_user.bookings
+    # @bookings =current_user.bookings
+    @bookings = policy_scope(Booking)
   end
-
 end
