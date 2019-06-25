9f08d8d2502ed4da38d8569b15d464b93a50b2cc
Axel Marko <axkmar@gmail.com>
2019-06-25 14:29:40 +0300

    Added reviews with image and name

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :performance
+  belongs_to :user
   validates :description, presence: true
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :performances, dependent: :destroy
   has_many :bookings
+  has_many :reviews, dependent: :destroy
 
   mount_uploader :photo, PhotoUploader
 
