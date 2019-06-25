a555108cd2a62f0fa146f8a91ec37f6b7a83b107
romy koenig <romyg.koenig@gmail.com>
2019-06-25 19:06:41 +0300

    add map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import 'select2/dist/css/select2.css';
 import "../plugins/init_flatpickr"
 import { initStarRating } from '../plugins/init_star_rating';
 import { initSelect2 } from '../components/init_select2';
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import { initMapbox } from '../plugins/init_mapbox';
 
 initSelect2();
 initStarRating();
-
+initMapbox();
