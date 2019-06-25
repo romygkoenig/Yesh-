9bcf237340dccebf314132f9c04300ba4fd86318
romy koenig <romyg.koenig@gmail.com>
2019-06-25 11:13:23 +0300

    add photo for user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
